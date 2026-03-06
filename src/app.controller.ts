888389c211a390adb387511972e93c5d33e23c5b
dfx_17 <rossohinpavel@yandex.ru>
2026-03-06 14:13:23 +0300

    swagger description

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { TypedRoute } from "@nestia/core";
 import { AppService } from "./app.service";
 import { Controller, Get } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 
-
+@ApiTags('App')
 @Controller()
 export class AppController {
 
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * @summary Пингуем сервис.
+   * @returns pong
+   */
   @TypedRoute.Get()
   getHello(): string {
     return this.appService.getHello();
